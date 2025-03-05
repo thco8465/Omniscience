ebe3a72d772a332c340fae3844717b058328f1e9
root <root@DESKTOP-G2HOFND.>
2025-03-05 09:10:27 -0700

    10000

--- backend/server.mjs
+++ backend/server.mjs
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 app.use('/leaderboard', leaderboardRoutes);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
